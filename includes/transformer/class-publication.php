55f940bd8cc4224b5d1fbdb874e1cea0d72e5feb
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-02-13 09:20:53 +0100

    Fix publication record: use 'name' instead of 'displayName'
    The site.standard.publication lexicon requires a 'name' field,
    not 'displayName'.

--- includes/transformer/class-publication.php
+++ includes/transformer/class-publication.php
@@ -33,7 +33,7 @@ class Publication extends Base {
 		$record = array(
 			'$type'       => 'site.standard.publication',
 			'url'         => home_url( '/' ),
-			'displayName' => get_bloginfo( 'name' ),
+			'name'        => get_bloginfo( 'name' ),
 			'description' => get_bloginfo( 'description' ),
 		);
 
